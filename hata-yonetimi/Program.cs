0c3f7d5fcfa1d7ff9367f99fca0c59466d446d29
canucar7 <74326628+canucar7@users.noreply.github.com>
2022-07-20 20:28:05 +0300

    Add files via upload

---
+++ hata-yonetimi/Program.cs
@@ -0,0 +1,50 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Bir sayı giriniz : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayi :" + sayi);
+        }
+        catch(Exception ex){
+            Console.WriteLine("Hata: " +ex.Message.ToString());
+        }
+        finally
+        {
+            Console.WriteLine("İşlem devam. ");
+        }
+
+
+        try
+        {
+            //int a = int.Parse(null);
+            //int b = int.Parse("test");
+            int c = int.Parse("-2000000000000");
+
+        }
+        catch (ArgumentNullException ex )
+        {
+            Console.WriteLine("Boş değer girdiniz  ");
+            Console.WriteLine(ex);
+        }
+        catch(FormatException ex)
+        {
+            Console.WriteLine("Yanlış veri tipi girdiniz  ");
+            Console.WriteLine(ex);
+        }
+        catch(OverflowException ex)
+        {
+            Console.WriteLine("Çok büyük ya da çok küçük bir değer girdnizi  ");
+            Console.WriteLine(ex);
+        }
+        finally
+        {
+             Console.WriteLine("İşlem tamam");
+        }
+
+
+
+    }
+}
