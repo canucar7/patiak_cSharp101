bf5cbcdf8b9724d4aa971f15b540c32d3e579453
canucar7 <74326628+canucar7@users.noreply.github.com>
2022-07-20 19:58:55 +0300

    Add files via upload

---
+++ tip-donusumleri/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        byte a=5;
+        sbyte b=30;
+        short c=10;
+        int d=a+b+c;
+        Console.WriteLine("d:" + d);
+        long h=d;
+        Console.WriteLine("h:" + h);
+        float i=h;
+        Console.WriteLine("i:" + i);
+        string e="can";
+        char f= 'k';
+        object g= e+f+d;
+        Console.WriteLine("g:" + g);
+    }
+}
