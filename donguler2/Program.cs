123aede2064f8d97d3810b998498fdc66493aea8
canucar7 <74326628+canucar7@users.noreply.github.com>
2022-07-22 02:09:58 +0300

    Add files via upload

---
+++ donguler2/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        /*
+        Console.WriteLine("Bir sayi giriniz : ");
+        int num = int.Parse(Console.ReadLine());
+
+        int count = 1;
+        int sum = 0;
+
+        while (count <= num)
+        {
+            sum += count ;
+            count ++ ;
+        }
+
+        Console.WriteLine(sum/num);
+        */
+
+
+        char ch = 'a';
+        while (ch < 'z')
+        {
+            Console.Write(ch);
+            ch ++ ;
+        }
+
+        Console.WriteLine(" ");
+
+        string[] cars = {"BMW", "Mercedes" , "Audi", "Nissan"};
+        foreach (var car in cars)
+        {
+            Console.WriteLine(car);
+        }
+
+    }
+}
