d63a92e28db31b05eae4613876843fbed8af86f4
canucar7 <74326628+canucar7@users.noreply.github.com>
2022-07-22 01:00:36 +0300

    Add files via upload

---
+++ if-else/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+        int time = DateTime.Now.Hour;
+        Console.WriteLine("Saat: " + time);
+        
+        if (time >= 6 && time < 12 )
+        {
+            Console.WriteLine("Günaydın ");
+        }
+        else if(time <=18)
+        {
+            Console.WriteLine("İyi günler ");
+        }
+        else
+        {
+            Console.WriteLine("İyi akşamlar ");
+        }
+
+
+        string str = time <= 18 ? "İyi günler " : "İyi akşamlar" ;
+        Console.WriteLine(str);
+
+        string srr = time >=6 && time <12 ? "Günaydın" : time <18 ? "İyi günler " : "İyi akşamlar";
+        Console.WriteLine(str);
+
+
+
+    }
+}
