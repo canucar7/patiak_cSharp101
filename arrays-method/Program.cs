4f4ae8da627bd4d36ca872569f139dc6120a48b5
canucar7 <74326628+canucar7@users.noreply.github.com>
2022-07-23 20:51:33 +0300

    Add files via upload

---
+++ arrays-method/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] sayiDizisi = {23,12,4,86,72,3,11,17};
+
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+     
+
+        Console.WriteLine("*********");
+
+
+        Array.Sort(sayiDizisi);
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+        
+
+        Console.WriteLine("*********");
+
+
+        Array.Clear(sayiDizisi,3,3);
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+
+         Console.WriteLine("*********");    
+
+         Array.Reverse(sayiDizisi);
+          foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+
+
+        Console.WriteLine("*********");  
+
+        Console.WriteLine(Array.IndexOf(sayiDizisi,11));
+
+
+        Console.WriteLine("*********");  
+        Array.Resize<int>(ref sayiDizisi,9);
+
+        sayiDizisi[8] = 99;
+        foreach (var sayi in sayiDizisi)
+            Console.WriteLine(sayi);
+
+
+
+
+
+
+    }
+}
