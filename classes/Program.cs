e6fee5832ebcfcefcd05276169302904c055855c
canucar7 <74326628+canucar7@users.noreply.github.com>
2022-07-29 23:27:13 +0300

    Add files via upload

---
+++ classes/Program.cs
@@ -0,0 +1,45 @@
+﻿    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Calısan calısan1 = new Calısan();
+            calısan1.Ad="Murat";
+            calısan1.Soyad="Kürkçü";
+            calısan1.No=234561;
+            calısan1.Depertmant="İnsan Kaynakları";
+            calısan1.CalısanBilgileri();
+
+            Console.WriteLine("*********");
+
+            Calısan calısan2 = new Calısan();
+            calısan2.Ad="Selin";
+            calısan2.Soyad="Mutlu";
+            calısan2.No=387162;
+            calısan2.Depertmant="Satın Alma";
+            calısan2.CalısanBilgileri();
+
+            
+
+
+
+        }
+    }
+
+    class Calısan
+    {
+        public string Ad;
+        
+        public string Soyad;
+
+        public int No;
+
+        public string Depertmant;
+
+        public void CalısanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı : {0}",Ad);
+            Console.WriteLine("Çalışanın Soyadı : {0}",Soyad);
+            Console.WriteLine("Çalışanın Numarası : {0}",No);
+            Console.WriteLine("Çalışanın Depertmanı : {0}",Depertmant);
+        }
+    }
