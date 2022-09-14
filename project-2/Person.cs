364d5478c093df3dcfb6deb9bee08d4baded62c6
canucar7 <74326628+canucar7@users.noreply.github.com>
2022-09-15 01:28:58 +0300

    Add files via upload

---
+++ project-2/Person.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+
+namespace project_2
+{
+    public class person
+    {
+        private List<TodoPersonModel> personn;
+
+        public person()
+        {
+            personn = new List<TodoPersonModel>();
+        }
+
+        public void Add(string no, string isim, string soyisim)
+        {
+            TodoPersonModel personModel = new TodoPersonModel();
+            personModel.No1 = no;
+            personModel.İsim1 = isim;
+            personModel.Soyisim1 = soyisim;
+            personn.Add(personModel);
+        }
+
+        public string Find(string sayi)
+        {
+            string result = "";
+
+            foreach (var item in personn)
+            {
+                if(item.No1 == sayi)
+                {
+                    result = item.İsim1 + " " + item.Soyisim1; 
+                }
+            }
+            return result;
+        }
+
+        public bool varmı(string sayi)
+        {
+            bool result = false;
+
+            foreach (var item in personn)
+            {
+                if(item.No1 == sayi)
+                {
+                    result = true; 
+                }
+            }
+            return result;
+        }
+    }
+}
